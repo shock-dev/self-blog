86de623c64b26d37dc959fcc4b5130d6afae5755
Andrey <shock.work@mail.ru>
2021-05-09 17:13:48 +0300

    change template for auth form

--- components/AuthForm/index.tsx
+++ components/AuthForm/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import styles from './AuthForm.module.scss';
 
 interface AuthFormProps {
+  title: string
   children: React.ReactNode
 }
 
-export default function AuthForm({ children }: AuthFormProps) {
+export default function AuthForm({ title, children }: AuthFormProps) {
   return (
     <div className={styles.form}>
-      {children}
+      <h2 className={styles.title}>
+        {title}
+      </h2>
+      <div>
+        {children}
+      </div>
     </div>
   );
 }
