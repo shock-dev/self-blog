ef1fd973886c5b9a866ed114b8bfaf4723356d12
Andrey <shock.work@mail.ru>
2021-05-10 12:22:13 +0300

    set up login form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,34 +1,70 @@
 import React from 'react';
 import Head from 'next/head';
+import { useFormik } from 'formik';
+import validationSchema from '../validation/auth/login';
 import AuthLayout from '../layouts/AuthLayout';
 import AuthForm from '../components/AuthForm';
 import AuthField from '../components/AuthField';
 import Button from '../components/Button';
 import FormFooter from '../components/FormFooter';
 
+interface FormInputs {
+  username: string
+  password: string
+}
+
 export default function Login() {
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    errors,
+    touched,
+    handleBlur
+  } = useFormik<FormInputs>({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    validationSchema,
+    onSubmit: (data) => {
+      console.log(data);
+    }
+  });
+
   return (
     <AuthLayout>
       <Head>
         <title>Login</title>
       </Head>
-      <AuthForm title="Login">
+      <AuthForm
+        title="Login"
+        onSubmit={handleSubmit}
+      >
         <AuthField
           title="Username"
           placeholder="Type your username"
-          error={false}
-          message="Error message"
           icon="user"
+          name="username"
+          value={values.username}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.username && !!errors.username}
+          message={errors.username}
         />
         <AuthField
           title="Password"
           type="password"
           placeholder="Type your password"
-          error={false}
-          message="Error message"
           icon="lock"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={touched.password && !!errors.password}
+          message={errors.password}
         />
-        <Button>
+        <Button type="submit" onClick={handleSubmit}>
           Login
         </Button>
         <FormFooter
