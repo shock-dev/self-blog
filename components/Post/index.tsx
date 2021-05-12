2a2dc2444f5e3a051f8f75702858fac3655f0313
Andrey <shock.work@mail.ru>
2021-05-12 12:38:20 +0300

    prepare post component

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './Post.module.scss';
+import { IPost } from '../../store/posts/types';
 
-export default function Post() {
+interface PostProps {
+  id: IPost['_id']
+  title: IPost['title']
+  description: IPost['description']
+}
+
+export default function Post({
+  id,
+  title,
+  description
+}: PostProps) {
   return (
     <article className={styles.post}>
-      <Link href="/">
+      <Link href={`/post/${id}`}>
         <a className={styles.title}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo
+          {title}
         </a>
       </Link>
       <p className={styles.desc}>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
-        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+        {description}
       </p>
     </article>
   );
