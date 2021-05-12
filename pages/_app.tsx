c250fa72b939f83ac5f206e539f7871513ec6238
Andrey <shock.work@mail.ru>
2021-05-12 11:16:59 +0300

    create store and init in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import '../styles/index.scss';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default App;
