4f66da7529217509e836d5f2275a8479397b8e73
Andrey <shock.work@mail.ru>
2021-05-15 21:55:06 +0300

    new saga for getting user info

--- store/auth/saga.ts
+++ store/auth/saga.ts
@@ -2,29 +2,49 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import { SagaIterator } from 'redux-saga';
 import { AuthActionType } from './types';
 import AuthApi from '../../api/auth';
-import { setError } from './actions';
+import { setError, setIsAuth, setUserInfo } from './actions';
 
-function* login(action): SagaIterator {
+function* fetchLogin(action): SagaIterator {
   try {
     const { payload } = action;
     const { status, data } = yield call(AuthApi.login, payload);
 
-    if (status === 'ok') {
+    if (status === 'ok' && data) {
       localStorage.setItem('authToken', `Bearer ${data}`);
     }
   } catch (e) {
     const { message } = e.response.data;
 
     if (message) {
-      put(setError(message));
+      yield put(setError(message));
     } else {
-      put(setError('something went wrong, try again'));
+      yield put(setError('Something went wrong, try again'));
+    }
+  }
+}
+
+function* fetchUserInfo(): SagaIterator {
+  try {
+    const { status, data } = yield call(AuthApi.getMe);
+
+    if (status === 'ok' && data) {
+      yield put(setUserInfo(data));
+      yield put(setIsAuth(true));
+    }
+  } catch (e) {
+    const { error } = e.response.data;
+
+    if (error) {
+      yield put(setError(error));
+    } else {
+      yield put(setError('Something went wrong, try again'));
     }
   }
 }
 
 function* authSaga() {
-  yield takeEvery(AuthActionType.FETCH_LOGIN, login);
+  yield takeEvery(AuthActionType.FETCH_LOGIN, fetchLogin);
+  yield takeEvery(AuthActionType.FETCH_USER_INFO, fetchUserInfo);
 }
 
 export default authSaga;
