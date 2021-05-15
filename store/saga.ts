39872ef61793e182b33c111e58d1ba883f09e298
Andrey <shock.work@mail.ru>
2021-05-15 20:53:21 +0300

    add auth saga to root saga

--- store/saga.ts
+++ store/saga.ts
@@ -1,8 +1,10 @@
 import { all } from 'redux-saga/effects';
 import postsSaga from './posts/saga';
+import authSaga from './auth/saga';
 
 export default function* rootSaga() {
   yield all([
-    postsSaga()
+    postsSaga(),
+    authSaga()
   ]);
 }
