d6e623ed47a5b8459a4b3d50ca58087d6d781b75
Andrey <shock.work@mail.ru>
2021-05-15 21:54:18 +0300

    new method in auth api

--- api/auth.ts
+++ api/auth.ts
@@ -1,9 +1,14 @@
-import axios from 'axios';
+import axios from '../core/axios';
 import { LoginFormInputs } from '../pages/login';
 
 class AuthApi {
   login = async (payload: LoginFormInputs) => {
     const { data } = await axios.post(`http://localhost:5000/api/auth/login`, payload);
+    return data;
+  }
+
+  getMe = async () => {
+    const { data } = await axios.get(`http://localhost:5000/api/users/me`);
     return data;
   }
 }
