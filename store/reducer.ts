6c7c8677c7162f57b1108d546852981123bef3b5
Andrey <shock.work@mail.ru>
2021-05-15 20:52:37 +0300

    change root reducer

--- store/reducer.ts
+++ store/reducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { RootState } from './types';
 import posts from './posts/reducer';
+import user from './auth/reducer';
 
 const reducer = combineReducers<RootState>({
-  posts
+  posts,
+  user
 });
 
 export default reducer;
