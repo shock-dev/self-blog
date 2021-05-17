20bf78f0a57a3e656e2fae111c05e397b4278cfb
Andrey <shock.work@mail.ru>
2021-05-17 10:54:14 +0300

    add new actions for logout

--- store/auth/actions.ts
+++ store/auth/actions.ts
@@ -4,6 +4,10 @@ import { LoginFormInputs } from '../../pages/login';
 
 export const fetchLogin = (payload: LoginFormInputs) => action(AuthActionType.FETCH_LOGIN, payload);
 
+export const logoutRequest = () => action(AuthActionType.LOGOUT_REQUEST);
+
+export const logoutSuccess = () => action(AuthActionType.LOGOUT_SUCCESS);
+
 export const fetchUserInfo = () => action(AuthActionType.FETCH_USER_INFO);
 
 export const setUserInfo = (payload: IUser) => action(AuthActionType.SET_USER_INFO, payload);
