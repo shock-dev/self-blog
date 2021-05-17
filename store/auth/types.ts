324c2783f50846feb8d40fe0efe451675aa032e0
Andrey <shock.work@mail.ru>
2021-05-17 10:53:35 +0300

    types for logout

--- store/auth/types.ts
+++ store/auth/types.ts
@@ -13,6 +13,8 @@ export interface AuthState {
 export enum AuthActionType {
   FETCH_LOGIN = '@auth/FETCH_LOGIN',
   FETCH_USER_INFO = '@auth/FETCH_USER_INFO',
+  LOGOUT_REQUEST = '@auth/LOGOUT_REQUEST',
+  LOGOUT_SUCCESS = '@auth/LOGOUT_SUCCESS',
   SET_USER_INFO = '@auth/SET_USER_INFO',
   SET_IS_AUTH = '@auth/SET_IS_AUTH',
   SET_ERROR = '@auth/SET_ERROR'
