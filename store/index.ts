1011e4824d1231deb9965da464c41a8ee7fe57b6
Andrey <shock.work@mail.ru>
2021-05-23 21:13:13 +0300

    change creating store

--- store/index.ts
+++ store/index.ts
@@ -1,18 +1,18 @@
-import { applyMiddleware, createStore } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducer';
-import saga from './saga';
 
-const sagaMiddleware = createSagaMiddleware();
+import { createWrapper, MakeStore } from 'next-redux-wrapper';
+import { applyMiddleware, createStore } from 'redux';
+import { RootState } from './types';
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(
-    applyMiddleware(sagaMiddleware)
-  )
-);
+const bindMiddleware = (middleware) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension');
+    return composeWithDevTools(applyMiddleware(...middleware));
+  }
+  return applyMiddleware(...middleware);
+};
 
-sagaMiddleware.run(saga);
+export const makeStore: MakeStore<RootState> = () =>
+  createStore(reducer, bindMiddleware([]));
 
-export default store;
+export const wrapper = createWrapper<RootState>(makeStore, { debug: true });
