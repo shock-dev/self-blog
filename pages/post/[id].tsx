7dab247dd90ffd62c5398eb72d459df9c600f02f
Andrey <shock.work@mail.ru>
2021-05-24 13:33:53 +0300

    protect one post page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import MainLayout from '../../layouts/MainLayout';
 import PostsApi from '../../api/posts';
-import { IPost } from '../../store/posts/types';
+import withAuthSS from '../../hocs/withAuth';
 
 interface PostProps {
-  data: IPost
+  post: any
 }
 
-const Post = ({ data }: PostProps) => {
+const Post = ({ post }: PostProps) => {
   return (
     <MainLayout title="test">
-      {data._id}
-      <br/>
-      {data.title}
+      {post._id}
+      <br />
+      {post.title}
     </MainLayout>
   );
 };
 
-export async function getServerSideProps({ params }) {
+export const getServerSideProps = withAuthSS(async ({ params }) => {
   const { data } = await PostsApi.getOne(params.id);
   return {
-    props: { data }
+    props: {
+      post: data
+    }
   };
-}
+});
 
 export default Post;
