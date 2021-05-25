5410d0f6aaf3f3f6a293a97c4661a44bfa69c23d
Andrey <shock.work@mail.ru>
2021-05-25 13:23:22 +0300

    getting posts and output

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import MainLayout from '../layouts/MainLayout';
 import Post from '../components/Post';
 import withAuthSS from '../hocs/withAuth';
+import PostsApi from '../api/posts';
 import styles from '../styles/pages/index.module.scss';
 
-const Home = () => {
+interface HomeProps {
+  posts: any[]
+}
+
+const Home = ({
+  posts
+}: HomeProps) => {
   return (
     <MainLayout title="Home">
       <div className={styles.wrapper}>
         <main className={styles.main}>
-          <Post />
+          {posts.map((post) =>
+            <Post
+              key={post._id}
+              id={post._id}
+              title={post.title}
+              description={post.description}
+              imageUrl={post.imageUrl}
+              views={post.views}
+              author={post.user}
+            />
+          )}
         </main>
         <aside className={styles.aside}>
           test
@@ -21,4 +38,16 @@ const Home = () => {
 
 export default Home;
 
-export const getServerSideProps = withAuthSS();
+export const getServerSideProps = withAuthSS(async () => {
+  try {
+    const { data } = await PostsApi.getAll();
+
+    return {
+      props: { posts: data }
+    };
+  } catch (e) {
+    return {
+      props: { posts: [] }
+    };
+  }
+});
