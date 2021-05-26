60eff8c94801f3d588f509f715dbb57ca07561d5
Andrey <shock.work@mail.ru>
2021-05-26 18:41:18 +0300

    prepare new actions of comments

--- store/comments/action.ts
+++
@@ -1,5 +0,0 @@
-import { action } from 'typesafe-actions';
-import { CommentsActionType } from './types';
-import { IComment } from '../../types/comment';
-
-export const setComments = (payload: IComment[]) => action(CommentsActionType.SET_COMMENTS, payload);
