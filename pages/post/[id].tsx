3138e8fbb1baf21eff0f8c24224bf52d93cdbc60
Andrey <shock.work@mail.ru>
2021-05-26 16:59:43 +0300

    dispatch comments and output it in template

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -5,12 +5,17 @@ import withAuthSS from '../../hocs/withAuth';
 import Post from '../../components/Post';
 import Comments from '../../components/Comments';
 import { IPost } from '../../types/post';
+import { setComments } from '../../store/comments/action';
+import { useSelector } from 'react-redux';
+import { selectCommentsData } from '../../store/comments/selectors';
 
 interface PostProps {
   post: IPost
 }
 
 const PostPage = ({ post }: PostProps) => {
+  const comments = useSelector(selectCommentsData);
+
   return (
     <MainLayout title={post.title}>
       <Post
@@ -22,23 +27,30 @@ const PostPage = ({ post }: PostProps) => {
         views={post.views}
         user={post.user}
       />
-      <Comments count={post.comments.length}>
-        {post.comments.map((comment) =>
-          <Comments.Item
-            key={comment._id}
-            text={comment.text}
-            user={comment.user}
-          />
-        )}
-      </Comments>
+      {comments && (
+        <Comments count={post.comments.length}>
+          {comments.map((comment) =>
+            <Comments.Item
+              key={comment._id}
+              text={comment.text}
+              user={comment.user}
+            />
+          )}
+        </Comments>
+      )}
       <Comments.Form />
     </MainLayout>
   );
 };
 
-export const getServerSideProps = withAuthSS(async ({ params }) => {
+export const getServerSideProps = withAuthSS(async ({ params, store }) => {
   try {
-    const { data } = await PostsApi.getOne(params.id);
+    const { data }: { data: IPost } = await PostsApi.getOne(params.id);
+
+    if (data.comments.length) {
+      store.dispatch(setComments(data.comments));
+    }
+
     return {
       props: {
         post: data
