1041f72434f673424d3c53729176f7aac3dd37d8
Andrey <shock.work@mail.ru>
2021-05-26 13:55:40 +0300

    add template of comments form

---
+++ components/Comments/Form/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styles from './Form.module.scss';
+import { useFormik } from 'formik';
+import validationSchema from '../../../validation/post/comment';
+
+interface CommentFormInputs {
+  text: string
+}
+
+const Form = () => {
+  const {
+    handleSubmit,
+    handleChange,
+    values,
+    errors,
+    touched,
+    handleBlur
+  } = useFormik<CommentFormInputs>({
+    initialValues: {
+      text: ''
+    },
+    validationSchema,
+    onSubmit: (data) => {
+      console.log(data);
+    }
+  });
+
+  return (
+    <form
+      className={styles.form}
+      onSubmit={handleSubmit}
+    >
+      <h4 className={styles.title}>
+        Написать комментарий
+      </h4>
+      <div className={styles.wrapper}>
+        <div className={styles.body}>
+          <textarea
+            className={styles.textarea}
+            value={values.text}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="text"
+          />
+        </div>
+        {touched.text && !!errors.text && (
+          <p className={styles.error}>
+            {errors.text}
+          </p>
+        )}
+        <div className={styles.footer}>
+          <button
+            className={styles.sendBtn}
+            type="submit"
+          >
+            Отправить
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
