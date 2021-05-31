88d014cd0ab01d4fc58e6f1fec8fa6774f7f3918
Andrey <shock.work@mail.ru>
2021-05-31 15:46:16 +0300

    new selector for getting user error

--- store/auth/selectors.ts
+++ store/auth/selectors.ts
@@ -3,3 +3,5 @@ import { RootState } from '../types';
 export const selectAuth = (state: RootState) => state.user;
 
 export const selectIsAuth = (state: RootState) => selectAuth(state).isAuth;
+
+export const selectAuthError = (state: RootState) => selectAuth(state).error;
