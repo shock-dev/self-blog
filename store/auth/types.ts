6dc4abbb4aec5b78b20ed45b2095152782627d77
Andrey <shock.work@mail.ru>
2021-06-02 12:43:57 +0300

    add new enum for register and create action for it

--- store/auth/types.ts
+++ store/auth/types.ts
@@ -10,9 +10,16 @@ export interface AuthState {
 export enum AuthActionType {
   FETCH_LOGIN = '@auth/FETCH_LOGIN',
   FETCH_USER_INFO = '@auth/FETCH_USER_INFO',
+
+  REGISTER_REQUEST = '@auth/REGISTER_REQUEST',
+  REGISTER_SUCCESS = '@auth/REGISTER_SUCCESS',
+
   LOGOUT_REQUEST = '@auth/FETCH_LOGOUT',
   LOGOUT_SUCCESS = '@auth/LOGOUT_SUCCESS',
+
   SET_USER_INFO = '@auth/SET_USER_INFO',
   SET_IS_AUTH = '@auth/SET_IS_AUTH',
-  SET_ERROR = '@auth/SET_ERROR'
+  SET_ERROR = '@auth/SET_ERROR',
+
+  CLEAR_FIELDS = '@auth/CLEAR_FIELDS'
 }
