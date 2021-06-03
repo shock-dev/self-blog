d4ced53c8650d8ef83e891a5172e2fd1ef341dcc
Andrey <shock.work@mail.ru>
2021-06-03 19:59:42 +0300

    validate fullname

--- validation/auth/register.ts
+++ validation/auth/register.ts
@@ -1,18 +1,26 @@
 import * as Yup from 'yup';
 import { ValidationConfig } from '../config';
 
-export default Yup.object({
+export const InfoStepValidation = Yup.object({
   email: Yup
     .string()
     .email(ValidationConfig.Email)
+    .required(ValidationConfig.Required),
+
+  fullname: Yup
+    .string()
+    .min(3, 'Минимальная длина имени 3 символа')
+    .max(30, 'Полное имя не должен превышать 30 символов')
     .required(ValidationConfig.Required),
 
   username: Yup
     .string()
     .min(3, 'Минимальная длина username 3 символа')
     .max(16, 'Username не должен превышать 16 символов')
-    .required(ValidationConfig.Required),
+    .required(ValidationConfig.Required)
+});
 
+export const PasswordStepValidation = Yup.object({
   password: Yup
     .string()
     .min(6, 'Минимальная длина пароля 6 символов')
