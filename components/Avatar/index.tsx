e2b85c8e14316d0bd37e25b53227b19241dd6153
Andrey <shock.work@mail.ru>
2021-06-06 20:07:59 +0300

    output letter in avatar if user doesnt have avatar url

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -1,5 +1,6 @@
 import React, { CSSProperties } from 'react';
 import styles from './Avatar.module.scss';
+import cn from 'classnames';
 
 interface AvatarProps {
   url: string
@@ -12,18 +13,35 @@ interface AvatarProps {
 
 const Avatar = ({
   url,
-  alt,
+  username,
   width = 240,
   height = 240,
   type = 'square',
   additionalStyles = {}
 }: AvatarProps) => {
+  // FIXME: Можно улучшить, попробовать без svg
+
   const avatarStyles = {
     width: `${width}px`,
     height: `${height}px`,
     borderRadius: `${type === 'circle' ? '50%' : '4px'}`,
     ...additionalStyles
   };
+
+  if (!url) {
+    const firstLetter = username?.charAt(0).toUpperCase();
+
+    return (
+      <div
+        className={cn(styles.wrapper, styles.svg)}
+        style={avatarStyles}
+      >
+        <svg className={styles.svg} viewBox="0 0 56 32">
+          <text fill="#fff" className={styles.letter} x="6.16" y="11">{firstLetter}</text>
+        </svg>
+      </div>
+    );
+  }
 
   return (
     <div
