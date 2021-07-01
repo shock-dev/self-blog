68b543df6a5cb88d04d88a490be234df0b5e1d65
Andrey <shock.work@mail.ru>
2021-07-01 21:09:02 +0300

    add template for reminder component

---
+++ components/Reminder/index.tsx
@@ -0,0 +1,25 @@
+import React, { CSSProperties } from 'react';
+import s from './Reminder.module.scss';
+import Link from 'next/link';
+
+interface ReminderProps {
+  text: string
+  styles?: CSSProperties
+}
+
+const Reminder = ({
+  text,
+  styles = {}
+}: ReminderProps) => {
+  return (
+    <p className={s.alert} style={styles}>
+      <Link href="/login">
+        <a className={s.link}>
+          Войдите в систему
+        </a>
+      </Link> чтобы {text}
+    </p>
+  );
+};
+
+export default Reminder;
