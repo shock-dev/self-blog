9071535956956c1824becdc71a44550e34e1aea6
Andrey <shock.work@mail.ru>
2021-07-05 18:58:07 +0300

    change logic of markdown output component

--- components/MarkdownOutput/index.tsx
+++ components/MarkdownOutput/index.tsx
@@ -3,15 +3,27 @@ import ReactMarkdown from 'react-markdown';
 import 'github-markdown-css/github-markdown.css';
 
 interface MarkdownOutputProps {
-  title: string
-  description: string
+  title?: string
+  description?: string
 }
 
 const MarkdownOutput = ({
   title,
   description
 }: MarkdownOutputProps) => {
-  const markdownText = `# ${title}\n ${description}`;
+  let markdownText: string;
+
+  if (title && !description) {
+    markdownText = `# ${title}`;
+  }
+
+  if (!title && description) {
+    markdownText = description;
+  }
+
+  if (title && description) {
+    markdownText = `# ${title}\n ${description}`;
+  }
 
   return (
     <div className="markdown-body">
