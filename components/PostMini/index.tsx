6496f91b407e0c36a4c878887748098428bd98c6
Andrey <shock.work@mail.ru>
2021-07-06 05:46:36 +0300

    add image to post mini component

--- components/PostMini/index.tsx
+++ components/PostMini/index.tsx
@@ -21,58 +21,64 @@ const PostMini = ({
   title,
   createdAt,
   user,
+  imageUrl = '',
   customStyles = {}
 }: PostMiniProps) => {
   const postUrl = `/post/${id}`;
 
   return (
     <article className={styles.post} style={customStyles}>
-      <div className={styles.header}>
-        <Avatar
-          url={user.avatarUrl}
-          width={40}
-          height={40}
-          additionalStyles={{ marginRight: '10px' }}
-          username={user.username}
-          type="circle"
-        />
-        <div className={styles.userWrapper}>
-          <Link href={`/user/${user._id}`}>
-            <a>
-              <span className={styles.fullname}>
-                {user.name} {user.surname}
-              </span>
-              <span className={styles.username}>
-                @{user.username}
-              </span>
-            </a>
-          </Link>
-          <div className={styles.time}>
-            {getDifference(createdAt)} назад
+      {!!imageUrl && (
+        <img src={imageUrl} className={styles.image} alt="" />
+      )}
+      <div className={styles.inner}>
+        <div className={styles.header}>
+          <Avatar
+            url={user.avatarUrl}
+            width={40}
+            height={40}
+            additionalStyles={{ marginRight: '10px' }}
+            username={user.username}
+            type="circle"
+          />
+          <div className={styles.userWrapper}>
+            <Link href={`/user/${user._id}`}>
+              <a>
+                <span className={styles.fullname}>
+                  {user.name} {user.surname}
+                </span>
+                <span className={styles.username}>
+                  @{user.username}
+                </span>
+              </a>
+            </Link>
+            <div className={styles.time}>
+              {getDifference(createdAt)} назад
+            </div>
           </div>
         </div>
+        <Link href={postUrl}>
+          <a>
+            <MarkdownOutput title={title} />
+          </a>
+        </Link>
+        <ul className={styles.categories}>
+          <li className={styles.category}>
+            <Link href="/">
+              <a className={styles.categoryLink}>
+                #Компьютерное железо
+              </a>
+            </Link>
+          </li>
+          <li className={styles.category}>
+            <Link href="/">
+              <a className={styles.categoryLink}>
+                #Презентации
+              </a>
+            </Link>
+          </li>
+        </ul>
       </div>
-      <Link href={postUrl}>
-        <a>
-          <MarkdownOutput title={title} />
-        </a>
-      </Link>
-      <ul className={styles.categories}>
-        <li className={styles.category}>
-          <Link href="/">
-            <a className={styles.categoryLink}>
-              #Компьютерное железо
-            </a>
-          </Link>
-        </li>
-        <li className={styles.category}>
-          <Link href="/">
-            <a className={styles.categoryLink}>
-              #Презентации
-            </a>
-          </Link>
-        </li>
-      </ul>
     </article>
   );
 };
