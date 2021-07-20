3d5f00fb600db9486c90347e4237c640f459acf8
Andrey <shock.work@mail.ru>
2021-07-20 13:27:01 +0300

    create spinner component

---
+++ components/Spinner/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './index.module.scss';
+
+const Spinner = () => {
+  return (
+    <div className={s.ldsRing}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default Spinner;
