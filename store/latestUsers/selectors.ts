aea7282ee535c1fd7e0502da8966f9c9c8c0a2aa
Andrey <shock.work@mail.ru>
2021-07-27 19:26:03 +0300

    delete store of last users

--- store/latestUsers/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { RootState } from '../types';
-
-export const selectLatestUser = (state: RootState) => state.latestUsers;
-
-export const selectLatestUserItems = (state: RootState) => selectLatestUser(state).items;
-
-export const selectLatestUserLoading = (state: RootState) => selectLatestUser(state).loading;
-
-export const selectLatestUserError = (state: RootState) => selectLatestUser(state).error;
