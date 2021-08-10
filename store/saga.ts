b50b974df7cded40a23ae8f0edf7aa8104241518
Andrey <shock.work@mail.ru>
2021-08-10 20:30:41 +0300

    destroy store

--- store/saga.ts
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-import authSaga from './auth/saga';
-
-export default function* rootSaga() {
-  yield all([
-    authSaga()
-  ]);
-}
--- store/types.ts
+++
@@ -1,5 +0,0 @@
-import { AuthState } from './auth/types';
-
-export interface RootState {
-  user: AuthState
-}
